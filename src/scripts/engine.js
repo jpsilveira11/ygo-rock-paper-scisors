0d6121f1d25addfc9b0fea4007d2ffa36e2ec293
jp <jpespechit@gmail.com>
2023-11-29 20:25:23 -0300

    drawing cards and previewing them

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,7 +4,7 @@ const state={
         opponentScore:0,
         scoreBox:document.getElementById('score'),
     },
-    cardSprites:{
+    card:{
         art:document.getElementById("card-art"),
         name:document.getElementById("card-name"),
         type:document.getElementById("card-type"),
@@ -17,7 +17,9 @@ const state={
         button:document.getElementById("next-match"),
     },
 };
+
 const pathTo="./src/assets/icons/";
+// const matchup={}
 const cards=[
     {
         id:0,
@@ -46,10 +48,62 @@ const cards=[
 ]
 
 const players={
-thePlayer:"player-cards",
+player:"player-field-card",
+opponent:"opponent-field-card",
 
+}
+const side={
+player:"player-cards",
+opponent:"opponent-cards",
+};
+
+
+async function getARandomCard(){
+    const index=Math.floor(Math.random()*cards.length);
+    return cards[index].id;
+}
+
+async function showCard(cardID){
+    state.card.art.src=cards[cardID].src;
+    state.card.name.innerText=cards[cardID].name;
+    state.card.type.innerText=`Attribute: ${cards[cardID].type}`;
+console.log(state.card.art.src);
+console.log(state.card.name.innerText);
+console.log(state.card.type.innerText);
+}
+
+async function getCardArt(cardID,theSide){
+const cardArt=document.createElement("img");
+cardArt.setAttribute("src","./src/assets/icons/card-back.png");
+cardArt.setAttribute("height","100px");
+cardArt.setAttribute("data-id",cardID);
+cardArt.classList.add("card");
+if(theSide===side.player){
+
+    cardArt.addEventListener("mouseover",()=>{
+        showCard(cardID);
+    });
+
+    cardArt.addEventListener("click",()=>{
+    //setCard(cardArt.getAttribute("data-id"));
+    });
+}
+
+return cardArt;
 }
 
 
-function run(){}
+async function draw(cards,side){
+    for(let index=0;index<cards;index++){
+        const randomCard=await getARandomCard();
+        const cardBack=await getCardArt(randomCard,side);
+        document.getElementById(side).appendChild(cardBack); 
+}
+
+}
+
+function run(){
+draw(5,side.player);
+draw(5,side.opponent);
+}
 run();
